c7bb3c15a0076532d7c38d1956a863916a296e72
NguyenVanVuong <vuongitspkt@gmail.com>
2017-05-10 21:53:36 +0700

    Update dbconection.java

--- springboot-jsp-master/src/main/java/com/conection/dbconection.java
+++ springboot-jsp-master/src/main/java/com/conection/dbconection.java
@@ -4,9 +4,7 @@ public class dbconection {
 	public String name="root";
 	public String pass="121212";
 	
-	// String sql = String.format(
-		//        "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
-		 //          + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "dtdmnhom3","baitapnhom3-sql:asia-northeast1:dtdmnhom3");
+	
 	String url = "jdbc:mysql://127.0.0.1:3306/sakila";
 	public String getURL()
 	{
