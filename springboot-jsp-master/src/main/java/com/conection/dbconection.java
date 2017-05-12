9f169c5131e194068676b0d9953bb973a07124d1
NguyenVanVuong <vuongitspkt@gmail.com>
2017-05-13 00:14:41 +0700

    Update dbconection.java

--- springboot-jsp-master/src/main/java/com/conection/dbconection.java
+++ springboot-jsp-master/src/main/java/com/conection/dbconection.java
@@ -1,12 +1,12 @@
 package com.conection;
 
 public class dbconection {
-	public String name="Postgres";
-	public String pass="vuong120992";
+	public String name="root";
+	public String pass="120992";
 	
 	 String url = String.format(
 		        "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
-		            + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "Postgres","vuong-app212:us-central1:cloudcpt151");
+		            + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "edit","vuong-app212:asia-northeast1:vuong-app212");
 	public String getURL()
 	{
 		return url;
